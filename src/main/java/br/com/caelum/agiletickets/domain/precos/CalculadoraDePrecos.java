f621dbc84bbeaa8ba39563d0b824f7028cd43c83
Maykon <maykonhs@gmail.com>
2015-11-25 20:37:16 -0200

    extraindo metodos na calculadoraDePrecos

--- src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
+++ src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
@@ -12,14 +12,14 @@ public class CalculadoraDePrecos {
 		
 		if(sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.CINEMA) || sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.SHOW)) {
 			//quando estiver acabando os ingressos... 
-			if((sessao.getTotalIngressos() - sessao.getIngressosReservados()) / sessao.getTotalIngressos().doubleValue() <= 0.05) { 
-				preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
+			if(getPorcentagemDisponivel(sessao) <= 0.05) { 
+				preco = getPrecoComTaxa(sessao, 0.10);
 			} else {
 				preco = sessao.getPreco();
 			}
 		} else if(sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.BALLET)) {
-			if((sessao.getTotalIngressos() - sessao.getIngressosReservados()) / sessao.getTotalIngressos().doubleValue() <= 0.50) { 
-				preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(0.20)));
+			if(getPorcentagemDisponivel(sessao) <= 0.50) { 
+				preco = getPrecoComTaxa(sessao, 0.20);
 			} else {
 				preco = sessao.getPreco();
 			}
@@ -28,8 +28,8 @@ public class CalculadoraDePrecos {
 				preco = preco.add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
 			}
 		} else if(sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.ORQUESTRA)) {
-			if((sessao.getTotalIngressos() - sessao.getIngressosReservados()) / sessao.getTotalIngressos().doubleValue() <= 0.50) { 
-				preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(0.20)));
+			if(getPorcentagemDisponivel(sessao) <= 0.50) { 
+				preco = getPrecoComTaxa(sessao, 0.20);
 			} else {
 				preco = sessao.getPreco();
 			}
@@ -43,6 +43,18 @@ public class CalculadoraDePrecos {
 		} 
 
 		return preco.multiply(BigDecimal.valueOf(quantidade));
+	}
+
+	private static BigDecimal getPrecoComTaxa(Sessao sessao, double taxa) {
+		return sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(taxa)));
+	}
+
+	private static double getPorcentagemDisponivel(Sessao sessao) {
+		return getIngressosDisponiveis(sessao) / sessao.getTotalIngressos().doubleValue();
+	}
+
+	private static int getIngressosDisponiveis(Sessao sessao) {
+		return sessao.getTotalIngressos() - sessao.getIngressosReservados();
 	}
 
 }
